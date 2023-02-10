eb176322f751be547b76e87c621f58643443b7ff
idlewith <newellzhou@163.com>
2023-02-10 20:38:11 +0800

    set base url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { searchPlugin } from '@vuepress/plugin-search'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 
 export default defineUserConfig({
-    // base: '/knowledge/',
+    base: '/knowledge/',
     locales: {
         "/": {
             lang: 'en-US',
